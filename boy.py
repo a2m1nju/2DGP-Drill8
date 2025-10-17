7ae9facadbefb90e28027e306e3e4db46a9d2468
choi minju <chlminju32@gmail.com>
2025-10-17 21:00:28 +0900

    a 키 입력 감지 함수 추가

--- boy.py
+++ boy.py
@@ -1,5 +1,5 @@
 from pico2d import load_image, get_time
-from sdl2 import SDL_KEYDOWN, SDLK_SPACE, SDLK_RIGHT, SDL_KEYUP, SDLK_LEFT
+from sdl2 import SDL_KEYDOWN, SDLK_SPACE, SDLK_RIGHT, SDL_KEYUP, SDLK_LEFT, SDLK_a
 from state_machine import StateMachine
 
 def right_down(e):
@@ -19,6 +19,9 @@ def space_down(e):
 
 def time_out(e):
     return e[0] == 'TIMEOUT'
+
+def a_down(e):
+    return e[0] == 'INPUT' and e[1].type == SDL_KEYDOWN and e[1].key == SDLK_a
 
 class Run:
     def __init__(self, boy):
